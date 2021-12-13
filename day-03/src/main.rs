b8d67b31f3477d2acb76e2947afeeeec43079229
Patrick Christensen <patrick.christensen@eksponent.com>
2021-12-13 23:06:45 +0100

    Day 3

---
+++ day-03/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::{self,BufRead};
+
+fn main() -> io::Result<()>
+{
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+
+    let mut pos_y = 0;
+    let mut pos_x = 0;
+
+    for line in handle.lines() {
+        let line_str = line.unwrap();
+        let line_value:Vec<&str> = line_str.split_whitespace().collect();
+        let command = line_value[0];
+        let value = line_value[1].parse::<i32>().unwrap();
+
+        match command {
+            "forward" => pos_x += value,
+            "down" => pos_y -= value,
+            "up" => pos_y += value,
+            _ => {}
+        }
+    }
+
+    println!("{}", pos_x * (-1 * pos_y));
+
+    Ok(())
+}
